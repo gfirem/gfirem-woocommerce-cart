00523bc139b85399669bb188f15610087dee3733
Juan <jjrosalesuci@gmail.com>
2018-11-30 00:12:46 -0300

    cart-dependencies

--- classes/class-gfirem-woo-cart-dependencies.php
+++ classes/class-gfirem-woo-cart-dependencies.php
@@ -2,25 +2,25 @@
 /**
  * Checks if WooCommerce is enabled
  */
-class GFireMWooCartDependencies {
+class GFireMWooCartDependencies
+{
+    private static $active_plugins;
 
-	private static $active_plugins;
+    public static function init()
+    {
+        self::$active_plugins = (array) get_option('active_plugins', array());
 
-	public static function init() {
+        if (is_multisite()) {
+            self::$active_plugins = array_merge(self::$active_plugins, get_site_option('active_sitewide_plugins', array()));
+        }
+    }
 
-		self::$active_plugins = (array) get_option( 'active_plugins', array() );
+    public static function woocommerce_active_check()
+    {
+        if (! self::$active_plugins) {
+            self::init();
+        }
 
-		if ( is_multisite() )
-			self::$active_plugins = array_merge( self::$active_plugins, get_site_option( 'active_sitewide_plugins', array() ) );
-	}
-
-	public static function woocommerce_active_check() {
-
-		if ( ! self::$active_plugins ) self::init();
-
-		return in_array( 'woocommerce/woocommerce.php', self::$active_plugins ) || array_key_exists( 'woocommerce/woocommerce.php', self::$active_plugins );
-	}
-
+        return in_array('woocommerce/woocommerce.php', self::$active_plugins, true) || array_key_exists('woocommerce/woocommerce.php', self::$active_plugins);
+    }
 }
-
-
